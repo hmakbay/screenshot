1a7cdc2579bc93a3bcc878d3084f4ecd9663c019
Hanna375 <hannaakbay375@gmail.com>
2021-05-13 19:46:25 +0300

    filename with timestamp

--- src/deneme/Screenshot.java
+++ src/deneme/Screenshot.java
@@ -5,22 +5,26 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class Screenshot {
     public static void main(String[] args){
 
         try {
 
+            Thread.sleep(5000);
+            LocalDate myObj = LocalDate.now();
             Robot r = new Robot();
             Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
             Rectangle rect = new Rectangle(dim);
             BufferedImage buff = r.createScreenCapture(rect);
             ImageIO.write(buff, "jpg", new File("C:\\Users\\hanna\\OneDrive\\Masaüstü\\screenshot.jpg"));
+            System.out.println(myObj);
             System.out.println("deneme.Screenshot alındı");
         }
 
 
-        catch (AWTException | IOException ex){
+        catch (AWTException | IOException | InterruptedException ex){
 
             System.out.println(ex);
             System.out.println("hata");
